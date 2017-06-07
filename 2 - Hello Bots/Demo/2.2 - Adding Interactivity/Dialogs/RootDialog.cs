1e5048697911c1c841c0e650cb4d5eec61f3e70a
Brandon H <hurlburb@microsoft.com>
2017-06-07 13:52:28 -0700

    2.2 - add interactivity

---
+++ 2 - Hello Bots/Demo/2.2 - Adding Interactivity/Dialogs/RootDialog.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Threading.Tasks;
+
+namespace Bot_Application.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            PromptDialog.Text(context, NameEntered, @"Hi! What's your name?");
+
+            return Task.CompletedTask;
+        }
+
+        private async Task NameEntered(IDialogContext context, IAwaitable<string> result)
+        {
+            await context.PostAsync($@"Hi {await result}!");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
