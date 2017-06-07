e5b4c470f0b5a737cfaaa2badc7af79171c4e783
Brandon H <hurlburb@microsoft.com>
2017-06-07 13:51:14 -0700

    2.1 - create first bot

---
+++ 2 - Hello Bots/Demo/2.1 - Create first bot/Dialogs/RootDialog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Bot_Application.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            await context.PostAsync(@"Hello, Bots!");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
